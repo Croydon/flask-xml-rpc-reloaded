59976095da87ac84b0d7347087a9f14771da6e16
Croydon <cr0ydon@outlook.com>
2017-09-07 20:00:29 +0200

    More renamings

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
 """
-Flask-XML-RPC
+Flask-XML-RPC-Re
 -------------
-Flask-XML-RPC adds XML-RPC support to Flask. This way, you can provide simple
+Flask-XML-RPC-Re adds XML-RPC support to Flask. This way, you can provide simple
 APIs easily accessible from almost any programming language.
 
 Links
 `````
 
-* `documentation <http://packages.python.org/Flask-XML-RPC>`_
+* `documentation <http://packages.python.org/Flask-XML-RPC-Re>`_
 * `development version
-  <http://bitbucket.org/leafstorm/flask-xml-rpc/get/tip.gz#egg=Flask-XML-RPC-dev>`_
+  <http://bitbucket.org/leafstorm/flask-xml-rpc/get/tip.gz#egg=Flask-XML-RPC-Re-dev>`_
 
 
 """
 from setuptools import setup
 
-
 setup(
-    name='Flask-XML-RPC',
+    name='Flask-XML-RPC-Re',
     version='0.1.2',
-    url='http://bitbucket.org/leafstorm/flask-xml-rpc/',
+    url='https://github.com/Croydon/flask-xml-rpc-reloaded',
     license='MIT',
     author='Matthew "LeafStorm" Frazier',
-    author_email='leafstormrush@gmail.com',
+    author_email='flask@go-dev.de',
     description='Adds support for creating XML-RPC APIs to Flask',
     long_description=__doc__,
     packages=['flaskext'],
