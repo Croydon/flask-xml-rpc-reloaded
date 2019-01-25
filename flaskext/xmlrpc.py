52abb9d4402b76a8540feb370884ee14e4200273
Aurélien Bompard <aurelien@bompard.org>
2019-01-25 14:06:04 +0100

    Wrong import in the compatibility package
    The `import *` in the `flaskext` compatibility package is actually importing from the wrong module.

--- flaskext/xmlrpc.py
+++ flaskext/xmlrpc.py
@@ -3,4 +3,4 @@ import warnings
 warnings.warn("flaskext.xmlrpcre namespace is deprecated, please use flask_xmlrpcre namespace instead",
               DeprecationWarning)
 
-from flask_xmlrpcre import *
+from flask_xmlrpcre.xmlrpcre import *
